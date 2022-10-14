9f14fb3525599c26b5b60649350dc6ff7d71d9f9
snehadole156 <113990426+snehadole156@users.noreply.github.com>
2022-10-14 18:13:44 +0530

    Create Question_5.java

---
+++ 13-10-2022/Question_5.java
@@ -0,0 +1,20 @@
+/*
+  Write a Java program to display the following pattern.
+
+   J    a   v     v  a                                                  
+   J   a a   v   v  a a                                                 
+J  J  aaaaa   V V  aaaaa                                                
+ JJ  a     a   V  a     a
+ 
+ */
+public class PatternJ    //create class
+{
+ 
+    public static void main(String[] args) {
+
+        System.out.println("   J    a   v     v  a ");     //for printing the statement
+        System.out.println("   J   a a   v   v  a a");
+        System.out.println("J  J  aaaaa   V V  aaaaa");
+        System.out.println(" JJ  a     a   V  a     a");
+    }
+}
